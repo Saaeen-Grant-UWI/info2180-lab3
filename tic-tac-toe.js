b61c8ac1821935c00251150dd5d905eccb5f84a8
Saaeen-Grant-UWI <saaeengrant.uwi@gmail.com>
2023-10-22 21:45:52 -0500

    Exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,35 @@
 const load = () => {
     const board =  document.getElementById("board")
-    board.querySelectorAll("div").forEach(x => x.setAttribute("class", "square"))
-    
+    const boardSquares = board.querySelectorAll("div")
+    let clickArray = []
+
+
+    boardSquares.forEach(element => element.setAttribute("class", "square"))
+
+    boardSquares.forEach(element => element.addEventListener("click", () => {
+
+        if ((clickArray.length == 0)) {
+            element.classList.add("X")
+            element.innerText = "X"
+        } 
+        else if (clickArray[0].classList.contains("X")) {
+            element.classList.remove("X")
+            element.classList.add("O")
+            element.innerText = "O"
+        } 
+        else {
+            element.classList.remove("O")
+            element.classList.add("X")
+            element.innerText = "X"
+        }
+
+        clickArray[0] = element  
+
+        
+      
+        
+    }))
+
 }
 
 document.addEventListener("DOMContentLoaded", load);
